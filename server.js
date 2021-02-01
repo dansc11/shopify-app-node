f5217c95b6e84666da6e4f237a838b387e0eadef
Paulo Margarido <paulo.margarido@shopify.com>
2021-02-01 10:30:41 -0500

    Setting up tutorial files

---
+++ server.js
@@ -0,0 +1,53 @@
+require('isomorphic-fetch');
+const dotenv = require('dotenv');
+const Koa = require('koa');
+const next = require('next');
+const { default: createShopifyAuth, initializeShopifyKoa } = require('@shopify/koa-shopify-auth');
+const { verifyRequest } = require('@shopify/koa-shopify-auth');
+const session = require('koa-session');
+const { default: Shopify, ApiVersion } = require('@shopify/shopify-api');
+
+dotenv.config();
+
+Shopify.Context.initialize({
+  API_KEY: process.env.SHOPIFY_API_KEY,
+  API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
+  SCOPES: process.env.SCOPES.split(","),
+  HOST_NAME: process.env.HOST.replace(/https:\/\//, ""),
+  API_VERSION: ApiVersion.October20,
+  IS_EMBEDDED_APP: true,
+  SESSION_STORAGE: new Shopify.Session.MemorySessionStorage(),
+});
+initializeShopifyKoa(Shopify.Context);
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = new Koa();
+  server.use(session({ secure: true, sameSite: 'none' }, server));
+  server.keys = [Shopify.Context.API_SECRET_KEY];
+
+  server.use(
+    createShopifyAuth({
+      afterAuth(ctx) {
+        const { shop, accessToken } = ctx.session;
+        ctx.redirect('/');
+      },
+    }),
+  );
+
+  server.use(verifyRequest());
+  server.use(async (ctx) => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+    ctx.res.statusCode = 200;
+    return
+  });
+
+  server.listen(port, () => {
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
